e7a702a462dd2ac6581d71b0a071e5b47bb9ce84
Joshua Reed <43630084+cellardoor2147@users.noreply.github.com>
2019-03-11 18:44:49 -0400

    directory change

---
+++ js/index.js
@@ -0,0 +1,28 @@
+console.log("CONNECTED");
+
+function setTime() {
+	var time = new Date();
+	var h = time.getHours();
+	var m = time.getMinutes();
+	var ampm;
+
+	if (h >= 12) {ampm = "PM";}
+	else {ampm = "AM";}
+
+	if (m < 10) {
+		m = "0" + m
+	}
+
+	if (h == 12) {
+		h = 12
+	}
+	else {
+		h %= 12
+	}
+
+	document.getElementById("startmenutime").innerHTML = 
+		h + ":" + m + " " + ampm;
+}
+
+window.onload = setTime
+window.setInterval(setTime, 500)
